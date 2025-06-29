e91875e70eaf8ee216627e9fbbb94c35a2333cb5
Ole Algoritme <ole@solgt.no>
2025-06-29 18:42:38 +0200

    fix audio conversion script

--- scripts/convert_audio.sh
+++ scripts/convert_audio.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Script to convert audio in MP4 files to AAC format with specific settings
-for f in *.mp4; do \
-  ffmpeg -i "$f" \
-    -c:v copy \
-    -c:a aac -b:a 256k -ac 2 -ar 48000 \
-    -movflags +faststart \
-    "${f%.mp4}-cast-ready.mp4"; \
+for f in *.mp4; do
+	ffmpeg -i "$f" \
+		-c:v copy \
+		-c:a aac -b:a 256k -ac 2 -ar 48000 \
+		-movflags +faststart \
+		"${f%.mp4}-cast-ready.mp4"
+done
